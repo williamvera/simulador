b3b2288ba21fd7230ea4e3e20bc95a247a08b8de
matrixsup <pedrotenac@gmail.com>
2015-10-21 20:41:00 -0500

    v2

---
+++ ClassLibrary1/Tasas.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassLibrary1
+{
+    public class Tasas
+    {
+        public int Cantidad
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public int Frecuencia
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public int Nombre_Entidad
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public int Numero_Tasas
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public EntidadesFinancieras EntidadesFinancieras
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public void Agregar()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Eliminar()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Listar()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Modificar()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
